23cc453b06f5c0ac943dfdb40c2e84192c1f7e60
emily <emilycaroline01042005@gmail.com>
2023-06-10 14:02:23 -0300

    terminado exercicio comentarios

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -7,4 +7,23 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    test('deve adicionar 2 comentarios ao post', ()=>{
+        render(<PostComment/>);
+        fireEvent.change(screen.getByTestId("coment-space"), {
+            target: {
+                value: "comentario numero 1"
+            }
+        })
+        fireEvent.click(screen.getByTestId("btn-comentar"))
+
+        fireEvent.change(screen.getByTestId("coment-space"), {
+            target: {
+                value: "comentario numero 2"
+            }
+        })
+        fireEvent.click(screen.getByTestId("btn-comentar"))
+
+        expect(screen.getAllByTestId("comentario")).toHaveLength(2)
+    })
 });
